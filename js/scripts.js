eb4b42cab9577e0bd364b3b9798a4411113656fd
molodoypatsan <89076616+molodoypatsan@users.noreply.github.com>
2021-10-11 21:29:57 +0700

    Сделали адаптив и анимацию

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ const menu = document.querySelector(".menu");
 let cost = 500;
 let totalPrice = 0;
 schemeSvg.addEventListener("click", (event) => {
-    if (!event.target.classList.contains('booked')) {
+    if (!event.target.classList.contains('booked') && !event.target.classList.contains('light')) {
         event.target.classList.toggle("active");
         let totalSeats = schemeSvg.querySelectorAll('.active').length;
         totalPrice = totalSeats * cost;
@@ -15,6 +15,5 @@ schemeSvg.addEventListener("click", (event) => {
 });
 
 menuButton.addEventListener("click", () => {
-    console.log("Кликнули по меню");
     menu.classList.toggle("is-open");
 });
